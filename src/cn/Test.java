95f1e4d797a7bcc0d549a2d0b20cd926941017aa
xzm <1548970696@qq.com>
2018-12-28 16:15:43 +0800

    xzm:提交测试项目文件

---
+++ src/cn/Test.java
@@ -0,0 +1,10 @@
+package cn;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.print("66666666");
+        for (int i=3;i<9;i++){
+            System.out.print(i);
+        }
+    }
+}
